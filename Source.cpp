7983fd25f7064f30e1f6dd3f7fd1c231c7abce00
Anatolii <99074480+AnatoliiMart@users.noreply.github.com>
2023-01-16 21:53:53 +0200

    need to fix random getter of elems

--- Source.cpp
+++ Source.cpp
@@ -86,10 +86,10 @@ int  Quewe::Extract()
 
 int Quewe::RandExtract(int& reelObj)
 {
-	int randIndex = 1 + rand() % 7;
+	int randIndex = rand() % 9;
 	for (size_t i = 0; i <= randIndex; i++)
 	{
-		if (i == randIndex/2)
+		if (i == randIndex)
 		{
 			reelObj = Extract();
 		}
@@ -116,26 +116,77 @@ int main()
 	const int lessCredits        = 5;
 	const int creditsToWin       = 10;
 	const int creditsForWinStuck = 15;
-	const int jackpotCredits     = 1000;
+	const int jackpotCredits     = 100;
 	PrintRules();
 	PauseAndClear();
-	std::cout << "----------------Menu----------------\n"
-		<< "1." << std::endl;
-
 	for (int i = 0; i < 9; i++)
 	{
 		r1->AddElem(rand() % 8);
 		r2->AddElem(rand() % 8);
 		r3->AddElem(rand() % 8);
 	}
-	r1->ShowReel();
-	r2->ShowReel();
-	r3->ShowReel();		
+	
+	int res1{0};
+    r1->RandExtract(res1);
+	int res2{0};
+    r2->RandExtract(res2);
+	int res3{0};
+    r3->RandExtract(res3);
+	int choose = 0;
+	int creditCurr = creditBegin;
+	do
+	{
+		std::cout << "----------------Menu----------------\n"
+			<< "Credits current: " << creditCurr << "\n"
+			<< "1.Spin\n"
+			<< "2.End game\n"
+			<< "Choose the action: ";
+		std::cin >> choose;
+		
+		switch (choose)
+		{
+		case 1:
+			std::cout << "Current combo: " << res1 << res2 << res3 << "\n\n";
+			creditCurr -= lessCredits;
+			if (res1 == res2 == res3 == 0 ||
+				res1 == res2 == res3 == 1 ||
+				res1 == res2 == res3 == 2 ||
+				res1 == res2 == res3 == 3 ||
+				res1 == res2 == res3 == 4 ||
+				res1 == res2 == res3 == 5 ||
+				res1 == res2 == res3 == 6)
 
-
-	delete r1;
-	delete r2;
-	delete r3;
+			{
+				creditCurr += creditsForWinStuck;
+				std::cout << " Win combo!!!\nYou won 15 credits." << std::endl;
+			}
+			else if (res2 == res3 == 0 || res1 == res2 == 0 || res1 == res3 == 0 ||
+				res2 == res3 == 1 || res1 == res2 == 1 || res1 == res3 == 1 ||
+				res2 == res3 == 2 || res1 == res2 == 2 || res1 == res3 == 2 ||
+				res2 == res3 == 3 || res1 == res2 == 3 || res1 == res3 == 3 ||
+				res2 == res3 == 4 || res1 == res2 == 4 || res1 == res3 == 4 ||
+				res2 == res3 == 5 || res1 == res2 == 5 || res1 == res3 == 5 ||
+				res2 == res3 == 6 || res1 == res2 == 6 || res1 == res3 == 6)
+			{
+				creditCurr += creditsToWin;
+				std::cout << " Win combo!!!\nYou won 10 credits." << std::endl;
+			}
+			else if (res1 == res2 == res3 == 7)
+			{
+				creditCurr += jackpotCredits;
+				std::cout << "JACKPOOOOOOOT!!!!!\n YOU WON 1000 CREDITS!!!!!!!!" << std::endl;
+			}
+			break;
+		case 2:
+			creditCurr = 0;
+			std::cout << "You decide to exit game( Good luck!" << std::endl;
+		}
+	} while (creditCurr > 0);
+	
+	if (creditCurr == 0)
+	{
+		std::cout << "There are not enough credits in your wallet(((" << std::endl;
+	}
 	return 0;
 }
 
@@ -145,7 +196,7 @@ void PrintRules()
 		<< "At the beginning of the game you have 100 coins.\n"
 		<< "For every non - winning scroll you lose 5 coins.\n"
 		<< "For every winning spin you get 10 coins\n"
-		<< "The winning combinations are: Nxx, NNx, xNx, xNN, xxN, NxN\n"
+		<< "The winning combinations are: NxN, NNx, xNN\n"
 		<< "Where x - some digit, you earned 10 credits.\n"
 		<< "For 111, 222, 333, 444, 555, 666 you earned 15 credits \n"
 		<< "For 777 you get the jackpot of 1000 coins\n"
